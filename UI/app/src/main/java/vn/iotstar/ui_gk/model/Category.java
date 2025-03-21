e3ebe92512b3cf3330ce373e89a4f738f78b5066
vophuhao <vophuhao2004@gmail.com>
2025-03-21 07:45:56 +0700

    category adapter

--- UI/app/src/main/java/vn/iotstar/ui_gk/model/Category.java
+++ UI/app/src/main/java/vn/iotstar/ui_gk/model/Category.java
@@ -1,4 +1,55 @@
 package vn.iotstar.ui_gk.model;
 
+import java.util.List;
+
 public class Category {
+
+    private int cateId;
+
+    private String cateName;
+
+    private String cateImage;
+
+    List<Product> listProduct;
+
+    public List<Product> getListProduct() {
+        return listProduct;
+    }
+
+    public void setListProduct(List<Product> listProduct) {
+        this.listProduct = listProduct;
+    }
+
+    public String getCateName() {
+        return cateName;
+    }
+
+    public void setCateName(String cateName) {
+        this.cateName = cateName;
+    }
+
+    public int getCateId() {
+        return cateId;
+    }
+
+    public void setCateId(int cateId) {
+        this.cateId = cateId;
+    }
+
+
+    public Category(int cateId, String cateName, List<Product> listProduct,String cateImage) {
+        this.cateId = cateId;
+        this.cateName = cateName;
+        this.listProduct = listProduct;
+        this.cateImage=cateImage;
+    }
+
+    public String getCateImage() {
+        return cateImage;
+    }
+
+    public void setCateImage(String cateImage) {
+        this.cateImage = cateImage;
+    }
+
 }
