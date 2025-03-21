474dab3aeb6b97a697d22dd9b4d040b7cae13908
Trần Trọng Nghĩa <163334466+TranTrongNghia1609@users.noreply.github.com>
2025-03-21 08:33:19 +0700

    nghia 22110380

--- UI/app/src/main/java/vn/iotstar/ui_gk/MainActivity.java
+++ UI/app/src/main/java/vn/iotstar/ui_gk/MainActivity.java
@@ -1,12 +1,16 @@
 package vn.iotstar.ui_gk;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import vn.iotstar.ui_gk.screen.activity.UserMainActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +19,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button btn_main = findViewById(R.id.button);
+        btn_main.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, UserMainActivity.class);
+            startActivity(intent);
         });
     }
 }
