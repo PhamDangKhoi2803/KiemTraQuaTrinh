e843703938216eddb7e3413eeadddf16ba16f5c4
Trần Trọng Nghĩa <163334466+TranTrongNghia1609@users.noreply.github.com>
2025-03-21 10:05:02 +0700

    API Service

--- UI/app/src/main/java/vn/iotstar/ui_gk/api/APIService.java
+++ UI/app/src/main/java/vn/iotstar/ui_gk/api/APIService.java
@@ -1,13 +1,18 @@
 package vn.iotstar.ui_gk.api;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import vn.iotstar.ui_gk.model.Category;
 import vn.iotstar.ui_gk.model.LoginRequest;
 
 public interface APIService {
 //    Nguyễn Văn Hùng - 22110338
     @POST("login")
     Call<String> login(@Body LoginRequest loginRequest);
-
+    @GET("/api/categories")
+    Call<List<Category>> getAllCate();
 }
