43aeb45dba187a0a14ae23b7dae2303ef8726e1d
vophuhao <vophuhao2004@gmail.com>
2025-03-21 09:38:53 +0700

    all category

--- UI/app/src/main/java/vn/iotstar/ui_gk/api/APIService.java
+++ UI/app/src/main/java/vn/iotstar/ui_gk/api/APIService.java
@@ -1,4 +1,13 @@
 package vn.iotstar.ui_gk.api;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import vn.iotstar.ui_gk.model.Category;
+
 public interface APIService {
+
+    @GET("/api/categories")
+    Call<List<Category>> getAllCate();
 }
