f0d77118161fac81d6087d8075b68d748156d798
Hùng Nguyễn <vanhung211104@gmail.com>
2025-03-21 08:19:19 +0700

    Viết LoginActivity

--- UI/app/src/main/java/vn/iotstar/ui_gk/api/APIService.java
+++ UI/app/src/main/java/vn/iotstar/ui_gk/api/APIService.java
@@ -1,4 +1,12 @@
 package vn.iotstar.ui_gk.api;
 
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import vn.iotstar.ui_gk.model.LoginRequest;
+
 public interface APIService {
+//    Nguyễn Văn Hùng - 22110338
+    @POST("auth/login")
+    Call<String> login(@Body LoginRequest loginRequest);
 }
---
+++ UI/app/src/main/java/vn/iotstar/ui_gk/model/LoginRequest.java
@@ -0,0 +1,12 @@
+package vn.iotstar.ui_gk.model;
+
+//Nguyễn Văn Hùng - 22110338
+public class LoginRequest {
+    private String email;
+    private String password;
+
+    public LoginRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
